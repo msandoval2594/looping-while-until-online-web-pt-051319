3207521b30534e735a306c4ed5f129717c87c6be
Michael Sandoval <msandoval2594@gmail.com>
2019-06-10 19:33:18 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,6 +4,6 @@ def using_until
   counter = 6
 until levitation_force == 10
 levitation_force += 1
-puts "Wingardium Leviosa is less than 10"    counter += 1
+puts "Wingardium Leviosa"    counter += 1
 end
 end
